9b7c90bb759d71e66063ac293ea2894dd660e190
Bulat Shakirzyanov <mallluhuct@gmail.com>
2015-04-03 13:44:59 -0700

    speed things up
    write everything in parallel, and wait for all writes to complete
    read everything in parallel and go over results afterwards

--- web/3-read-write/cassandra-ext.php
+++ web/3-read-write/cassandra-ext.php
@@ -5,24 +5,42 @@ require __DIR__ . '/../../vendor/autoload.php';
 $cluster = Cassandra::cluster()->build();
 $session   = $cluster->connect('ext');
 
-$res = array();
+// BEGIN WRITES
+$futures = array();
+$statement = new Cassandra\SimpleStatement("INSERT INTO user (id, fname, lname, description) VALUES (?, ?, ?, ?)");
+$options = new Cassandra\ExecutionOptions();
 for ($index = 0; $index < 100; $index++) {
     $id = rand(0, 1000000);
     $fname = md5($id);
     $lname = sha1($id);
     $description = $fname . $lname;
-    $statement = new Cassandra\SimpleStatement("INSERT INTO user (id, fname, lname, description) VALUES ($id, '".$fname."', '".$lname."', '".$description."')");
-    $session->executeAsync($statement);
+    $options->args = array($id, $fname, $lname, $description);
+    $futures[]= $session->executeAsync($statement, $options);
+}
 
-    $id = rand(0, 1000000);
-    $statement = new Cassandra\SimpleStatement("SELECT * FROM user WHERE id = $id");
-    $future    = $session->executeAsync($statement);
+// make sure all writes complete
+foreach ($futures as $future) {
+    $future->get();
+}
+var_dump($index);
+// END WRITES
+
+// BEGIN READS
+$futures = array();
+$statement = new Cassandra\SimpleStatement("SELECT * FROM user WHERE id = ?");
+for ($index = 0; $index < 100; $index++) {
+    $options->args = array(rand(0, 1000000));
+    $futures[]= $session->executeAsync($statement, $options);
+}
+
+$res = array();
+foreach ($futures as $future) {
     /** @var \Cassandra\Rows $result */
-    $result    = $future->get();
+    $result = $future->get();
     if($result->count()){
         $res[] =  $result->current();
     }
 }
 
-var_dump($index);
 var_dump(count($res));
+// END READS
