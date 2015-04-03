edb3e4cef9843e726d5f331e50d088fc66cb087f
Sacha Morard <sacha.morard@moarty.com>
2015-04-03 17:16:27 +0200

    remove vendors. Install it with composer

--- vendor/datastax/php-driver/ext/util/inet.h
+++
@@ -1,8 +0,0 @@
-#ifndef PHP_CASSANDRA_UTIL_INET_H
-#define PHP_CASSANDRA_UTIL_INET_H
-
-void php_cassandra_format_address(CassInet inet, char** out);
-int php_cassandra_parse_ip_address(char* in, CassInet* inet);
-
-
-#endif /* PHP_CASSANDRA_UTIL_INET_H */
--- vendor/datastax/php-driver/ext/util/uuid_gen.h
+++
@@ -1,8 +0,0 @@
-#ifndef PHP_CASSANDRA_UUID_GEN_H
-#define PHP_CASSANDRA_UUID_GEN_H
-
-void php_cassandra_uuid_generate_random(CassUuid* out);
-void php_cassandra_uuid_generate_time(CassUuid* out);
-void php_cassandra_uuid_generate_from_time(long timestamp, CassUuid* out);
-
-#endif /* PHP_CASSANDRA_UUID_GEN_H */
