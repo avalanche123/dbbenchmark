fdf55b68ac6b548f2921b97803952a459c5f8f4e
Bulat Shakirzyanov <mallluhuct@gmail.com>
2015-04-03 13:33:31 -0700

    make sure all writes complete
    write in bulk and wait for all writes to finish
    also use positional arguments instead of a creating statements

--- web/1-write/cassandra-ext.php
+++ web/1-write/cassandra-ext.php
@@ -5,13 +5,21 @@ require __DIR__ . '/../../vendor/autoload.php';
 $cluster = Cassandra::cluster()->build();
 $session   = $cluster->connect('ext');
 
+$futures = array();
+$statement = new Cassandra\SimpleStatement("INSERT INTO user (id, fname, lname, description) VALUES (?, ?, ?, ?)");
+$options = new Cassandra\ExecutionOptions();
 for ($index = 0; $index < 100; $index++) {
     $id = rand(0, 1000000);
     $fname = md5($id);
     $lname = sha1($id);
     $description = $fname . $lname;
-    $statement = new Cassandra\SimpleStatement("INSERT INTO user (id, fname, lname, description) VALUES ($id, '".$fname."', '".$lname."', '".$description."')");
-    $session->executeAsync($statement);
+    $options->args = array($id, $fname, $lname, $description);
+    $futures[]= $session->executeAsync($statement, $options);
+}
+
+foreach ($futures as $future) {
+    // wait for the INSERT to complete
+    $future->get();
 }
 
 var_dump($index);
